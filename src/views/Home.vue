48055db9c7fcba96d157df3f8becc6c53568ba36
gedha rizka <gedharizka@gmail.com>
2020-03-01 19:15:44 +0700

    make component HeroCarousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,8 @@
 <template>
   <div class="home">
     <HeaderStore />
-    <!-- Hero Section Begin -->
-    <section class="hero-section">
-      <carousel class="hero-items" :items="1" :nav="false" :autoplay="true">
-
-        <div class="single-hero-items set-bg" style="background-image:url('/img/hero-1.jpg')">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="single-hero-items set-bg" style="background-image:url('/img/hero-2.jpg')">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="single-hero-items set-bg" style="background-image:url('/img/hero-3.jpg')">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5">
-                <span>Bag,kids</span>
-                <h1>Black friday</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
-                <a href="#" class="primary-btn">Shop Now</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </carousel>
-    </section>
-    <!-- Hero Section End -->
+    <HeroCarousel />
+    
 
   </div>
 </template>
@@ -52,13 +11,13 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderStore from '@/components/HeaderStore.vue'
-import carousel from 'vue-owl-carousel'
+import HeroCarousel from '@/components/HeroCarousel.vue'
 
 export default {
   name: 'Home',
   components: {
     HeaderStore,
-    carousel
+    HeroCarousel
   }
 }
 </script>
