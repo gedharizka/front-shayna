6cb43938da059b4629c87370711e4aabe34d353a
gedha rizka <gedharizka@gmail.com>
2020-03-01 19:32:36 +0700

    add womman banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <HeaderStore />
     <HeroCarousel />
-    
+    <WomanBanner />
+   
 
   </div>
 </template>
@@ -12,12 +13,14 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderStore from '@/components/HeaderStore.vue'
 import HeroCarousel from '@/components/HeroCarousel.vue'
+import WomanBanner from '@/components/WomanBanner.vue'
 
 export default {
   name: 'Home',
   components: {
     HeaderStore,
-    HeroCarousel
+    HeroCarousel,
+    WomanBanner,
   }
 }
 </script>
