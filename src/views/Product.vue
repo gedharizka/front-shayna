29365d06c301efb33bcc1553071de66c92a2b3f9
gedha rizka <gedharizka@gmail.com>
2020-03-08 14:52:54 +0700

    Product vue dynamic thumbs

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -3,7 +3,7 @@
     <HeaderStore />
 
     <!-- Breadcrumb Section Begin -->
-    <div class="breacrumb-section">
+    <div class="breacrumb-section text-left">
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
@@ -27,23 +27,23 @@
             <div class="row">
               <div class="col-lg-6">
                 <div class="product-pic-zoom">
-                  <img class="product-big-img" src="img/mickey1.jpg" alt />
+                  <img class="product-big-img" :src=gambar_default alt />
                 </div>
                 <div class="product-thumbs">
-                  <carousel class="product-thumbs-track ps-slider" :dots="false" :items="3" :nav="false" :autoplay="true" :loop="true">
-                    <div class="pt active" data-imgbigurl="img/mickey1.jpg">
+                  <carousel class="product-thumbs-track ps-slider" :dots="false" :items="3" :nav="false">
+                    <div class="pt" @click="changeImage(thumbs[0])" :class="thumbs[0] == gambar_default ? 'active' :'' " >
                       <img src="img/mickey1.jpg" alt />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey2.jpg">
+                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == gambar_default ? 'active' :'' " >
                       <img src="img/mickey2.jpg" alt />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey3.jpg">
+                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == gambar_default ? 'active' :'' " >
                       <img src="img/mickey3.jpg" alt />
                     </div>
 
-                    <div class="pt" data-imgbigurl="img/mickey4.jpg">
+                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == gambar_default ? 'active' :'' " >
                       <img src="img/mickey4.jpg" alt />
                     </div>
                   </carousel><!-- Carousel -->
@@ -51,7 +51,7 @@
                 </div>
               </div>
               <div class="col-lg-6">
-                <div class="product-details">
+                <div class="product-details text-left">
                   <div class="pd-title">
                     <span>oranges</span>
                     <h3>Pure Pineapple</h3>
@@ -96,7 +96,24 @@ export default {
     HeaderStore,
     Footer,
     carousel
+  },
+  data (){
+    return {
+      gambar_default: "img/mickey1.jpg",
+      thumbs:[
+        "img/mickey1.jpg",
+        "img/mickey2.jpg",
+        "img/mickey3.jpg",
+        "img/mickey4.jpg"
+      ]
+    }
+  },
+  methods:{
+    changeImage(urlImage){
+      this.gambar_default = urlImage;
+    }
   }
+
 };
 </script>
 
