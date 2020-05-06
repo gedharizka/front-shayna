01c44cd966126dd86138150383578033f0f657bd
Gedha Rizka <gedharizka@gmail.com>
2020-05-06 22:52:42 +0700

    title product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -53,18 +53,12 @@
               <div class="col-lg-6">
                 <div class="product-details text-left">
                   <div class="pd-title">
-                    <span>oranges</span>
-                    <h3>Pure Pineapple</h3>
+                    <span>{{productDetails.type}}</span>
+                    <h3>{{productDetails.name}}</h3>
                   </div>
                   <div class="pd-desc">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.</p>
-                    <p>
-                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                      Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                      Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                      impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                    </p>
-                    <h4>$495.00</h4>
+                    <p>{{productDetails.description}}</p>
+                    <h4>${{productDetails.price}}</h4>
                   </div>
                   <div class="quantity">
                     <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
@@ -92,7 +86,7 @@ import HeaderStore from "@/components/HeaderStore.vue";
 import Footer from "@/components/Footer.vue";
 import carousel from 'vue-owl-carousel';
 import RelatedProduct from '@/components/RelatedProduct.vue'
-
+import axios from 'axios';
 
 export default {
   name: "Product",
@@ -111,15 +105,27 @@ export default {
         "img/mickey3.jpg",
         "img/mickey4.jpg"
       ],
-      idProducts: this.$route.params.id
+     productDetails:[]
     }
   },
   methods:{
     changeImage(urlImage){
       this.gambar_default = urlImage;
-      console.log(this.idProducts)
     }
+  },
+
+   mounted(){
+    axios
+      .get("http://127.0.0.1:8000/api/products",{
+        params:{
+          id: this.$route.params.id
+        }
+      })
+      // .get("http://shayna-backend.belajarkoding.com/api/products")
+      .then(res=> (this.productDetails = res.data.data))
+      .catch(err => console.log(err))
   }
+  
 
 };
 </script>
